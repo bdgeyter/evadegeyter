20f713e686966d92d03505f6895ffce700abb043
Bram De Geyter <bdgeyter@gmail.com>
2016-09-30 16:06:32 +0200

    minified scripts

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const gutil = require('gulp-util');
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename'); 
 
 const siteRoot = '_site';
 const cssFiles = 'assets/css/*.?(s)css';
@@ -14,6 +16,15 @@ gulp.task('css', () => {
     .pipe(sass())
     .pipe(concat('all.css'))
     .pipe(gulp.dest('assets'));
+});
+
+gulp.task('minify', function () {
+   gulp.src([
+        './assets/js/scripts.js',
+   ])
+      .pipe(uglify())
+      .pipe(rename('scripts.min.js'))
+      .pipe(gulp.dest('./assets/js/')) // It will create folder client.min.js
 });
 
 gulp.task('jekyll', () => {
@@ -43,4 +54,4 @@ gulp.task('serve', () => {
   //gulp.watch(cssFiles, ['css']);
 });
 
-gulp.task('default', [ 'jekyll', 'serve']);
+gulp.task('default', ['minify', 'jekyll', 'serve']);
