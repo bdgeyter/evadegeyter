198f0b4eb1dffd648ee310c36cdc862020213568
Bram De Geyter <bdgeyter@gmail.com>
2016-09-30 14:06:05 +0200

    less dependencies, cdn use and improved page speed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,168 +3,192 @@
 
 $(document).ready(function() {
 
-    "use strict";
-  
-    //------- MODAL ---------//
-    if (window.location.hash.indexOf('project') > 0) {
-      $(window.location.hash).modal('show');
+  "use strict";
+
+  //------- MODAL ---------//
+  if (window.location.hash.indexOf('project') > 0) {
+    $(window.location.hash).modal('show');
+  }
+
+  $('a[data-toggle="modal"]').click(function(){
+    window.location.hash = $(this).attr('href');
+  });
+
+  $('.modal').on('hidden.bs.modal', function () {
+    var original = window.location.href.substr(0, window.location.href.indexOf('#'))
+    history.replaceState({}, document.title, original);
+  });
+
+  window.onhashchange = function(){
+    if (!window.location.hash) {
+      $('.modal').modal('hide');
     }
+  }
 
-    $('a[data-toggle="modal"]').click(function(){
-        window.location.hash = $(this).attr('href');
-    });
-  
-    $('.modal').on('hidden.bs.modal', function () {
-        var original = window.location.href.substr(0, window.location.href.indexOf('#'))
-        history.replaceState({}, document.title, original);
-    });
-     
-    window.onhashchange = function(){
-      if (!window.location.hash) {
-        $('.modal').modal('hide');
-      }
+  //------- SAFER MAILTO BUTTON ---//
+  $('.btn.mailto').click(function(){
+    this.preventDefault;
+    var username = 'hallo',
+        domain = 'evadegeyter.be';
+    location.href = 'mailto:' + username + '@' + domain;
+  });
+
+  //------- SITE LOADER ---//
+
+  jQuery(".spinner").fadeOut();
+  jQuery(".loading-mask").delay(400).fadeOut("slow");
+
+
+  //---------MENU CHANGES BLACK ----//
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= window.innerHeight ) {
+      $(".header-fixed").addClass("header-fixed-black");
+    } else {
+      $(".header-fixed").removeClass("header-fixed-black");
     }
+  });
 
-    //------- SAFER MAILTO BUTTON ---//
-    $('.btn.mailto').click(function(){
-      this.preventDefault;
-      var username = 'hallo',
-          domain = 'evadegeyter.be';
-      location.href = 'mailto:' + username + '@' + domain;
-    });
-
-    //------- SITE LOADER ---//
-
-    jQuery(".spinner").fadeOut();
-    jQuery(".loading-mask").delay(400).fadeOut("slow");
+  /*---- NAV MENU ---- */
 
 
-    //---------MENU CHANGES BLACK ----//
+  $('.site-menu').on('click', function() {
+    $(this).toggleClass('triggered');
+    $('.sidebar-menu').addClass('sidebar-open');
+    $('.page-wrapper').addClass('page-wrapper-side');
+  });
 
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= window.innerHeight ) {
-            $(".header-fixed").addClass("header-fixed-black");
-        } else {
-            $(".header-fixed").removeClass("header-fixed-black");
-        }
-    });
+  $('.menu-close').on('click', function() {
+    $('.sidebar-menu').removeClass('sidebar-open');
+    $('.page-wrapper').removeClass('page-wrapper-side');
+  });
 
-    /*---- NAV MENU ---- */
+  $('.smenu li a').on('click', function() {
+    $('.sidebar-menu').removeClass('sidebar-open');
+    $('.page-wrapper').removeClass('page-wrapper-side');
+  });
 
+  $('#page').on('click', function() {
+    $('.sidebar-menu').removeClass('sidebar-open');
+    $('.page-wrapper').removeClass('page-wrapper-side');
+  });
 
-    $('.site-menu').on('click', function() {
-        $(this).toggleClass('triggered');
-        $('.sidebar-menu').addClass('sidebar-open');
-        $('.page-wrapper').addClass('page-wrapper-side');
-    });
+  // Add smooth scrolling on all links inside the navbar
+  $(".navigation a").on('click', function(event) {
 
-    $('.menu-close').on('click', function() {
-        $('.sidebar-menu').removeClass('sidebar-open');
-        $('.page-wrapper').removeClass('page-wrapper-side');
-    });
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
 
-    $('.smenu li a').on('click', function() {
-        $('.sidebar-menu').removeClass('sidebar-open');
-        $('.page-wrapper').removeClass('page-wrapper-side');
-    });
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-    $('#page').on('click', function() {
-        $('.sidebar-menu').removeClass('sidebar-open');
-        $('.page-wrapper').removeClass('page-wrapper-side');
-    });
+      // Store hash
+      var hash = this.hash;
 
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
 
-    
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
 
-    //------- JQUERY ISOTOPE ---//
+    } // End if
 
-
-    // external js: isotope.pkgd.js, classie.js
-
-    // ----- getText helper ----- //
-
-    var textProp = document.documentElement.textContent !== undefined ? 'textContent' : 'innerText';
-
-    function getText( elem ) {
-        return elem[ textProp ];
-    }
+  });
 
 
-    // init Isotope for Blog
+  //------- JQUERY ISOTOPE ---//
+
+
+  // external js: isotope.pkgd.js, classie.js
+
+  // ----- getText helper ----- //
+
+  var textProp = document.documentElement.textContent !== undefined ? 'textContent' : 'innerText';
+
+  function getText( elem ) {
+    return elem[ textProp ];
+  }
+
+
+  // init Isotope for Blog
   /*
     $('#isoBlog').isotope({
         itemSelector: '.isoBlog',
         gutter: 10
     });*/
 
-    // init Isotope for Portfolio with Filters
-    var iso = new Isotope( '#isotopeGrid', {
-        itemSelector: '.itemIso',
-        percentPosition: true,
-        masonry: {
-            columnWidth: '.grid-sizer',
-            gutter: 20, 
-        }
-    });
-
-
-    // filter functions
-    var filterFns = {
-        // show if number is greater than 50
-        numberGreaterThan50: function( itemElem ) {
-            var number = getText( itemElem.querySelector('.number') );
-            return parseInt( number, 10 ) > 50;
-        },
-        // show if name ends with -ium
-        ium: function( itemElem ) {
-            var name = getText( itemElem.querySelector('.name') );
-            return name.match( /ium$/ );
-        }
-    };
-
-    // bind filter button click
-    var filtersElem = document.querySelector('.filters');
-    eventie.bind( filtersElem, 'click', function( event ) {
-        // only work with buttons
-        if ( !matchesSelector( event.target, '.filter' ) ) {
-            return;
-        }
-        var filterValue = event.target.getAttribute('data-filter');
-        // use matching filter function
-        filterValue = filterFns[ filterValue ] || filterValue;
-        iso.arrange({ filter: filterValue });
-    });
-
-    // change is-checked class on buttons
-    var buttonGroups = document.querySelectorAll('.filters-group');
-    for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
-        var buttonGroup = buttonGroups[i];
-        radioButtonGroup( buttonGroup );
+  // init Isotope for Portfolio with Filters
+  var iso = new Isotope( '#isotopeGrid', {
+    itemSelector: '.itemIso',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer',
+      gutter: 20, 
     }
+  });
 
 
-
-    function radioButtonGroup( buttonGroup ) {
-        eventie.bind( buttonGroup, 'click', function( event ) {
-            // only work with buttons
-            if ( !matchesSelector( event.target, '.filter' ) ) {
-                return;
-            }
-            classie.remove( buttonGroup.querySelector('.is-checked'), 'is-checked' );
-            classie.add( event.target, 'is-checked' );
-        });
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function( itemElem ) {
+      var number = getText( itemElem.querySelector('.number') );
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function( itemElem ) {
+      var name = getText( itemElem.querySelector('.name') );
+      return name.match( /ium$/ );
     }
+  };
+
+  // bind filter button click
+  var filtersElem = document.querySelector('.filters');
+  eventie.bind( filtersElem, 'click', function( event ) {
+    // only work with buttons
+    if ( !matchesSelector( event.target, '.filter' ) ) {
+      return;
+    }
+    var filterValue = event.target.getAttribute('data-filter');
+    // use matching filter function
+    filterValue = filterFns[ filterValue ] || filterValue;
+    iso.arrange({ filter: filterValue });
+  });
+
+  // change is-checked class on buttons
+  var buttonGroups = document.querySelectorAll('.filters-group');
+  for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+    var buttonGroup = buttonGroups[i];
+    radioButtonGroup( buttonGroup );
+  }
+
+
+
+  function radioButtonGroup( buttonGroup ) {
+    eventie.bind( buttonGroup, 'click', function( event ) {
+      // only work with buttons
+      if ( !matchesSelector( event.target, '.filter' ) ) {
+        return;
+      }
+      classie.remove( buttonGroup.querySelector('.is-checked'), 'is-checked' );
+      classie.add( event.target, 'is-checked' );
+    });
+  }
 
   // ---- Typed ---- //
 
   $("#typed").typed({
-        stringsElement: $("#typed-strings"),
-        typeSpeed: 100,
-        loop: true,
-        startDelay: 1000,
-        backDelay:1000,
-    });
+    stringsElement: $("#typed-strings"),
+    typeSpeed: 100,
+    loop: true,
+    startDelay: 1000,
+    backDelay:1000,
+  });
 
   // ---- Gallery ---- //
   $(".gallery").justifiedGallery({
@@ -182,42 +206,42 @@ $(document).ready(function() {
 /*-- GOGGLE MAP
    –––––––––––––––––––––––––––––––––––––––––––––––––– --*/
 
-    function initMap() {
-      
-        // Basic options for a simple Google Map
-        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-        var mapOptions = {
-            // How zoomed in you want the map to start at (always required)
-            zoom: 9,
+function initMap() {
 
-            // The latitude and longitude to center the map (always required)
-            center: new google.maps.LatLng(51.0535, 3.7304), //Ghent
+  // Basic options for a simple Google Map
+  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+  var mapOptions = {
+    // How zoomed in you want the map to start at (always required)
+    zoom: 9,
 
-            // How you would like to style the map.
-            // This is where you would paste any style found on Snazzy Maps.
-            styles:
-            [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}],
+    // The latitude and longitude to center the map (always required)
+    center: new google.maps.LatLng(51.0535, 3.7304), //Ghent
 
-            //Other options
-            mapTypeControl: false,
-            draggable: false,
-            scaleControl: false,
-            scrollwheel: false,
-            navigationControl: false,
-            streetViewControl: false,
-        };
+    // How you would like to style the map.
+    // This is where you would paste any style found on Snazzy Maps.
+    styles:
+    [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}],
 
-        // Get the HTML DOM element that will contain your map
-        // We are using a div with id="map" seen below in the <body>
-        var mapElement = document.getElementById('map');
+    //Other options
+    mapTypeControl: false,
+    draggable: false,
+    scaleControl: false,
+    scrollwheel: false,
+    navigationControl: false,
+    streetViewControl: false,
+  };
 
-        // Create the Google Map using our element and options defined above
-        var map = new google.maps.Map(mapElement, mapOptions);
+  // Get the HTML DOM element that will contain your map
+  // We are using a div with id="map" seen below in the <body>
+  var mapElement = document.getElementById('map');
 
-        // Let's also add a marker while we're at it
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(51.0535, 3.7304),
-            map: map,
-            title: 'Snazzy!'
-        });
-    }
+  // Create the Google Map using our element and options defined above
+  var map = new google.maps.Map(mapElement, mapOptions);
+
+  // Let's also add a marker while we're at it
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(51.0535, 3.7304),
+    map: map,
+    title: 'Snazzy!'
+  });
+}
