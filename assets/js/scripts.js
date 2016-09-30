8ef46b059afbb9ae1d99f5cf9ea99493d6f3ce13
Bram De Geyter <bdgeyter@gmail.com>
2016-09-30 15:41:58 +0200

    fixes issue with isotope layout upon loading

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -123,62 +123,25 @@ $(document).ready(function() {
     });*/
 
   // init Isotope for Portfolio with Filters
-  var iso = new Isotope( '#isotopeGrid', {
-    itemSelector: '.itemIso',
-    percentPosition: true,
-    masonry: {
-      columnWidth: '.grid-sizer',
-      gutter: 20, 
-    }
-  });
+  var $grid = $('#isotopeGrid').imagesLoaded(function(){
+    $grid.isotope({
+      itemSelector: '.itemIso',
+      percentPosition: true,
+      masonry: {
+        columnWidth: '.grid-sizer',
+        gutter: 20, 
+      },
 
-
-  // filter functions
-  var filterFns = {
-    // show if number is greater than 50
-    numberGreaterThan50: function( itemElem ) {
-      var number = getText( itemElem.querySelector('.number') );
-      return parseInt( number, 10 ) > 50;
-    },
-    // show if name ends with -ium
-    ium: function( itemElem ) {
-      var name = getText( itemElem.querySelector('.name') );
-      return name.match( /ium$/ );
-    }
-  };
-
-  // bind filter button click
-  var filtersElem = document.querySelector('.filters');
-  eventie.bind( filtersElem, 'click', function( event ) {
-    // only work with buttons
-    if ( !matchesSelector( event.target, '.filter' ) ) {
-      return;
-    }
-    var filterValue = event.target.getAttribute('data-filter');
-    // use matching filter function
-    filterValue = filterFns[ filterValue ] || filterValue;
-    iso.arrange({ filter: filterValue });
-  });
-
-  // change is-checked class on buttons
-  var buttonGroups = document.querySelectorAll('.filters-group');
-  for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
-    var buttonGroup = buttonGroups[i];
-    radioButtonGroup( buttonGroup );
-  }
-
-
-
-  function radioButtonGroup( buttonGroup ) {
-    eventie.bind( buttonGroup, 'click', function( event ) {
-      // only work with buttons
-      if ( !matchesSelector( event.target, '.filter' ) ) {
-        return;
-      }
-      classie.remove( buttonGroup.querySelector('.is-checked'), 'is-checked' );
-      classie.add( event.target, 'is-checked' );
     });
-  }
+  });
+
+  // filter items on button click
+  $('.filters-group').on( 'click', 'li', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+    $('.filters-group > li').removeClass('is-checked');
+    $(this).addClass('is-checked');
+  });
 
   // ---- Typed ---- //
 
